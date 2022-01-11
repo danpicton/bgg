fa434cca6a3ce4bcb29b32269da4b347702f8734
Dan Picton <daniel.picton@gmail.com>
2022-01-11 07:11:57 +0000

    Fix warnings, remove commented out code

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 extern crate clap;
 extern crate reqwest;
 extern crate serde;
-// extern crate csv;
+
 
 #[macro_use]
 extern crate serde_derive;
 
 // use clap::{Arg, App, SubCommand};
-// use reqwest::Client;
-use std::{io::{copy, self, Write}, fmt::Display};
+use std::io;
 use anyhow::Result;
 use chrono::{Utc, Duration};
 
 use simple_logger::{SimpleLogger};
-
-
 
 // TODO: replace logging library with macro for verbose outputting
 
@@ -51,6 +48,7 @@ struct BoardGame {
     thumbnail_url: String,
 }
 
+
 fn boardgames_from_reader<T: io::Read>(mut rdr: csv::Reader<T>) -> Result<Vec<BoardGame>> {
     let mut boardgames = Vec::<BoardGame>::new();
 
@@ -62,35 +60,21 @@ fn boardgames_from_reader<T: io::Read>(mut rdr: csv::Reader<T>) -> Result<Vec<Bo
     Ok(boardgames)
 }
 
+#[allow(clippy::needless_question_mark)]
 fn download_csv (date_string: &str) -> Result<Vec<BoardGame>> {
     let url = format!("https://gitlab.com/recommend.games/bgg-ranking-historicals/-/raw/master/{}.csv", &date_string);
     log::info!("Attempting download: {}", &url);
     let resp = reqwest::blocking::get(url)?.text()?;
     
-    // TODO: Handle errors  here
     Ok(boardgames_from_reader(csv::Reader::from_reader(resp.as_bytes()))?)
-
 }
 
-
+#[allow(clippy::needless_question_mark)]
 fn read_file(file_name: &str) -> Result<Vec<BoardGame>> {
-
-    // let mut boardgames = Vec::<BoardGame>::new();
     let csv_file = std::fs::File::open(file_name)?;
-
-    // TODO: Handle errors  here
+    
     Ok(boardgames_from_reader(csv::Reader::from_reader(csv_file))?)
-
-    // for result in csv::Reader::from_reader(csv_file).deserialize() {
-    //     let boardgame: BoardGame = result?;
-    //     boardgames.push(boardgame);
-    // }
-
-    // Ok(boardgames)
 }
-
-
-
 
 fn main() -> Result<()>  {
     // Initialise UTC logger to obviate local time issue
@@ -109,15 +93,12 @@ fn main() -> Result<()>  {
     std::fs::create_dir_all(&config_path)?;
     log::info!("Config path created: {:?}", config_path);
 
-
-    let mut today_date = Utc::now();
-    
+    let today_date = Utc::now();
     
     // assumes that a file will exist by default (accommodate where it's been deleted manually)
     let mut recent_file: Option<String> = None;
 
     for i in 0..MAX_DL_DATE_LOOKBACKS {
-        // let file_date_string = file_date.format("%Y-%m-%d");
         let file_date = today_date - Duration::days(i);
         log::info!("iteration: {} date: {}", i, file_date.format("%Y-%m-%d"));
         let file_path = config_path.join(file_date.format("%Y-%m-%d.csv").to_string());
@@ -125,37 +106,29 @@ fn main() -> Result<()>  {
         log::info!("Looking for file: {:?}", &file_path);
         if std::path::Path::new(&file_path).exists() {
             log::info!("Recent file found: {:?}", &file_path);
-            // convert PathBuf to String
             recent_file = Some(file_path.into_os_string().into_string().unwrap());
             break;
         }
     }
     
-//https://stackoverflow.com/questions/51141672/how-do-i-ignore-an-error-returned-from-a-rust-function-and-proceed-regardless
+
 let mut boardgames: Vec<BoardGame> = Vec::<BoardGame>::new();
-// String
     match recent_file {
         Some(file_path) => {
             println!("Processing: {:?}", file_path);
             if let Ok(bgs) = read_file(file_path.as_str()) {
                 boardgames = bgs;
             }
-            // let csv_data = parse_csv(read_file(file_path.as_str())?);
-            // let csv_data = parse_csv(read_file(file_path.as_str())?.deserialize());
         },
         None => {
-            // unimplemented!("");
             let date_string = &today_date.format("%Y-%m-%d").to_string();
             if let Ok(bgs) = download_csv(date_string) {
                 let boardgames = bgs;
-                // let csv_data = csv_string.deserialize();
-                // log::info!("STILL GOT DATE STRING: {:?}",format!("{}.csv", date_string));
+
+                // TODO: create save CSV function
                 let output_file = config_path.join(format!("{}.csv", date_string));
-                // let mut out = std::fs::File::create(output_file)?;
-                // io::copy(&mut csv_data, &mut out)?;
-                // write!(out, "{}", boardgames);
                 let mut wtr =csv::Writer::from_path(output_file)?;
-                // wtr.write_record(&["ID","Name","Year","Rank","Average","Bayes average","Users rated","URL","Thumbnail"])?;
+    
                 for boardgame in boardgames {
                     wtr.serialize(boardgame)?;
                 }
@@ -166,7 +139,8 @@ let mut boardgames: Vec<BoardGame> = Vec::<BoardGame>::new();
             };
         },
     }
-    // println!("{:?}", boardgames);
+    
+    println!("{:?}", boardgames);
 
 
     Ok(())
